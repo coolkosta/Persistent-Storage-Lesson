143c95687ce9a9c2c637a0d3929ce3f6a9e596c8
coolkosta <kostia.borzakov@gmail.com>
2022-11-22 20:23:17 +0300

    DataStore

--- app/src/main/java/com/example/persistentstoragelesson/MainActivity.kt
+++ app/src/main/java/com/example/persistentstoragelesson/MainActivity.kt
@@ -8,14 +8,29 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.core.content.edit
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var editText: EditText
     private lateinit var button: Button
     private lateinit var textView: TextView
+    private lateinit var tvDataStore: TextView
 
     private lateinit var sharedPreferences: SharedPreferences
+    private val Context.dataStore by preferencesDataStore(name = "settings")
+
+    private object PreferencesKey {
+        val userInput = stringPreferencesKey("text_key_data_store")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +41,28 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.editText)
         button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
+        tvDataStore = findViewById(R.id.tvDataStore)
 
         button.setOnClickListener {
             val text = editText.text.toString()
+            // Сохранение в SharedPref
             saveString(text)
+            // Сохранение в DataStore
+            saveStringWithDataStore(text)
+
         }
 
         textView.text = getText()
+
+        val userPreferencesFlow: Flow<String> = dataStore.data.map { preferences ->
+            preferences[PreferencesKey.userInput] ?: ""
+        }
+
+        GlobalScope.async {
+            userPreferencesFlow.collect { it ->
+              tvDataStore.text = it
+            }
+        }
     }
 
     private fun saveString (text: String) {
@@ -44,6 +74,14 @@ class MainActivity : AppCompatActivity() {
     private fun getText(): String {
         val savedText = sharedPreferences.getString("text_key", "No Result")
         return savedText ?: ""
+    }
+
+    private fun saveStringWithDataStore(text: String) {
+        GlobalScope.async {
+            dataStore.edit { preferences ->
+                preferences[PreferencesKey.userInput] = text
+            }
+        }
     }
 
     companion object {
